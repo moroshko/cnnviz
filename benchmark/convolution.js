bed90ce78b7e05b57518e831bb5ff494325b9fff
Misha Moroshko <michael.moroshko@gmail.com>
2018-10-17 00:07:21 -0700

    Implement addPadding

--- benchmark/convolution.js
+++ benchmark/convolution.js
@@ -1,11 +1,12 @@
 const { performance } = require("perf_hooks");
-const { INPUT_WIDTH, INPUT_HEIGHT } = require("../utils/constants");
 const {
-  getConvolutionOutputDimensions,
-  convolve
-} = require("../utils/convolution");
+  INPUT_DISPLAY_WIDTH,
+  INPUT_DISPLAY_HEIGHT
+} = require("../utils/constants");
+const { getOutputDimensions } = require("../utils/shared");
+const { convolve } = require("../utils/convolution");
 
-const COUNT = 300;
+const COUNT = 100;
 
 function getRandomNumber(min, max) {
   return min + Math.floor(Math.random() * (max - min + 1));
@@ -23,8 +24,8 @@ function getRandomFilter(filterSize) {
 }
 
 function getRandomInputData() {
-  const length = (INPUT_WIDTH * INPUT_HEIGHT) << 2;
-  const inputData = new Array(length);
+  const length = (INPUT_DISPLAY_WIDTH * INPUT_DISPLAY_HEIGHT) << 2;
+  const inputData = new Uint8ClampedArray(length);
 
   for (let i = 0; i < length; i++) {
     inputData[i] = getRandomNumber(0, 255);
@@ -33,44 +34,58 @@ function getRandomInputData() {
   return inputData;
 }
 
+const strides = [1, 2, 4];
 const filterSizes = [1, 3, 5, 7];
 const filters = filterSizes.map(getRandomFilter);
 
-for (let i = 0; i < filterSizes.length; i++) {
-  const filterSize = filterSizes[i];
-  const filter = filters[i];
-  let totalTime = 0;
+strides.forEach(stride => {
+  filterSizes.forEach((filterSize, filterIndex) => {
+    const filter = filters[filterIndex];
+    let totalTime = 0;
 
-  for (let k = 0; k < COUNT; k++) {
-    const inputData = getRandomInputData();
-    const startTime = performance.now();
-    const { outputData } = convolve({
-      inputWidth: INPUT_WIDTH,
-      inputHeight: INPUT_HEIGHT,
-      inputData,
-      filterSize,
-      filter,
-      ...getConvolutionOutputDimensions({
-        inputWidth: INPUT_WIDTH,
-        inputHeight: INPUT_HEIGHT,
-        filterSize
-      })
-    });
-    const endTime = performance.now();
+    for (let k = 0; k < COUNT; k++) {
+      const inputData = getRandomInputData();
+      const startTime = performance.now();
+      const { outputData } = convolve({
+        inputWidth: INPUT_DISPLAY_WIDTH,
+        inputHeight: INPUT_DISPLAY_HEIGHT,
+        inputData,
+        filter,
+        filterSize,
+        stride,
+        ...getOutputDimensions({
+          inputWidth: INPUT_DISPLAY_WIDTH,
+          inputHeight: INPUT_DISPLAY_HEIGHT,
+          filterSize,
+          stride
+        })
+      });
+      const endTime = performance.now();
 
-    totalTime += endTime - startTime;
-  }
+      totalTime += endTime - startTime;
+    }
 
-  const averageTime = Math.round(totalTime / COUNT);
+    const averageTime = (totalTime / COUNT).toFixed(2);
 
-  console.log(`${filterSize}x${filterSize} filter: ${averageTime}ms`);
-}
+    console.log(
+      `filterSize = ${filterSize}, stride = ${stride} ===> ${averageTime}ms`
+    );
+  });
+});
 
 /*
-  const COUNT = 300;
+  const COUNT = 100;
 
-  1x1 filter: 10ms
-  3x3 filter: 30ms
-  5x5 filter: 70ms
-  7x7 filter: 130ms
+  filterSize = 1, stride = 1 ===> 10.94ms
+  filterSize = 3, stride = 1 ===> 28.57ms
+  filterSize = 5, stride = 1 ===> 64.64ms
+  filterSize = 7, stride = 1 ===> 122.84ms
+  filterSize = 1, stride = 2 ===> 2.94ms
+  filterSize = 3, stride = 2 ===> 7.61ms
+  filterSize = 5, stride = 2 ===> 17.30ms
+  filterSize = 7, stride = 2 ===> 31.40ms
+  filterSize = 1, stride = 4 ===> 0.46ms
+  filterSize = 3, stride = 4 ===> 1.66ms
+  filterSize = 5, stride = 4 ===> 3.88ms
+  filterSize = 7, stride = 4 ===> 7.56ms
 */
--- utils/shared.js
+++ utils/shared.js
@@ -1,13 +1,52 @@
 const { LAYER_TYPES } = require("./constants");
 
-function getOutputDimensions({ inputWidth, inputHeight, filterSize, stride }) {
+function getOutputDimensions({
+  inputWidth,
+  inputHeight,
+  filterSize,
+  padding = 0,
+  stride
+}) {
+  const twicePadding = padding << 1;
+
   return {
     // x << 0 is a faster version of Math.floor(x)
-    outputWidth: ((inputWidth - filterSize) / stride + 1) << 0,
-    outputHeight: ((inputHeight - filterSize) / stride + 1) << 0
+    outputWidth: ((inputWidth - filterSize + twicePadding) / stride + 1) << 0,
+    outputHeight: ((inputHeight - filterSize + twicePadding) / stride + 1) << 0
+  };
+}
+
+function addPadding({
+  inputWidth,
+  inputHeight,
+  inputData,
+  padding,
+  outputWidth,
+  outputHeight
+}) {
+  const outputSize = (outputWidth * outputHeight) << 2;
+  const outputData = new Uint8ClampedArray(outputSize);
+  let inputDataIndex = 0;
+  const inputDataIndexStep = inputWidth << 2;
+  let outputDataIndex = ((outputWidth + 1) * padding) << 2;
+  const outputDataIndexStep = outputWidth << 2;
+
+  for (let h = 0; h < inputHeight; h++) {
+    outputData.set(
+      inputData.subarray(inputDataIndex, inputDataIndex + inputDataIndexStep),
+      outputDataIndex
+    );
+
+    inputDataIndex += inputDataIndexStep;
+    outputDataIndex += outputDataIndexStep;
+  }
+
+  return {
+    outputData
   };
 }
 
 module.exports = {
-  getOutputDimensions
+  getOutputDimensions,
+  addPadding
 };
