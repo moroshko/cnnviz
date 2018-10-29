e097507ab06da67684aa55bee81d0387e84fb4cf
Misha Moroshko <michael.moroshko@gmail.com>
2018-10-28 21:45:55 -0700

    Add controlsReducer + tests

---
+++ utils/controlsReducer.js
@@ -0,0 +1,298 @@
+import {
+  INPUT_DISPLAY_WIDTH,
+  INPUT_DISPLAY_HEIGHT,
+  INPUT_TYPES,
+  LAYER_TYPES
+} from "./constants";
+import { getOutputDimensions } from "./shared";
+
+const IMAGES = [
+  {
+    src: "/static/digit_4.jpg",
+    scale: 16
+  },
+  {
+    src: "/static/Valley-Of-Gods-Photo-By-John-B-Mueller.jpg",
+    scale: 1
+  }
+];
+
+// prettier-ignore
+const INITIAL_CONV_FILTERS = [
+  {
+    name: 'Edge detection',
+    isEditable: false,
+    filter: [
+      0,  1, 0,
+      1, -4, 1,
+      0,  1, 0
+    ]
+  },
+  {
+    name: 'Blur',
+    isEditable: false,
+    filter: [
+      1, 1, 1, 1, 1,
+      1, 1, 1, 1, 1,
+      1, 1, 1, 1, 1,
+      1, 1, 1, 1, 1,
+      1, 1, 1, 1, 1,
+    ]
+  },
+  {
+    name: 'Sharpen',
+    isEditable: false,
+    filter: [
+       0, -1,  0,
+      -1,  5, -1,
+       0, -1,  0
+    ]
+  },
+  {
+    name: 'Custom (editable)',
+    isEditable: true,
+    filter: [
+       0, 0, 0,
+       0, 1, 0,
+       0, 0, 0
+    ]
+  },
+].map(convFilter => ({
+  ...convFilter,
+  filterSize: Math.sqrt(convFilter.filter.length),
+  errors: convFilter.filter.map(_ => false)
+}));
+
+// TODO: Search for duplicates of this function
+function getConvPadding({ convFilters, convFilterIndex, convStride }) {
+  return (convFilters[convFilterIndex].filterSize - convStride) >> 1;
+}
+
+// TODO: Search for duplicates of this function
+function getScale({ inputType, inputImage }) {
+  return inputType === INPUT_TYPES.IMAGE ? inputImage.scale : 1;
+}
+
+// TODO: Search for duplicates of this function
+function getLayerSpecificParams(state) {
+  const { layerType } = state;
+
+  switch (layerType) {
+    case LAYER_TYPES.CONV: {
+      const { convFilters, convFilterIndex, convPadding, convStride } = state;
+
+      return {
+        filterSize: convFilters[convFilterIndex].filterSize,
+        padding: convPadding,
+        stride: convStride
+      };
+    }
+
+    case LAYER_TYPES.POOL: {
+      const { poolFilterSize, poolStride } = state;
+
+      return {
+        filterSize: poolFilterSize,
+        padding: 0,
+        stride: poolStride
+      };
+    }
+
+    default: {
+      throw new Error(`Unknown layer type: ${layerType}`);
+    }
+  }
+}
+
+const initialControlsState = {
+  inputType: INPUT_TYPES.IMAGE,
+  inputImage: IMAGES[1],
+  hasRedChannel: true,
+  hasGreenChannel: true,
+  hasBlueChannel: true,
+  layerType: LAYER_TYPES.CONV,
+  convFilters: INITIAL_CONV_FILTERS,
+  convFilterIndex: 0,
+  convStride: 1,
+  poolFilterSize: 2,
+  poolStride: 2
+};
+
+initialControlsState.convPadding = getConvPadding(initialControlsState);
+initialControlsState.scale = getScale(initialControlsState);
+
+const {
+  outputWidth: outputDataWidth,
+  outputHeight: outputDataHeight
+} = getOutputDimensions({
+  inputWidth: INPUT_DISPLAY_WIDTH / initialControlsState.scale,
+  inputHeight: INPUT_DISPLAY_HEIGHT / initialControlsState.scale,
+  ...getLayerSpecificParams(initialControlsState)
+});
+
+initialControlsState.outputDataWidth = outputDataWidth;
+initialControlsState.outputDataHeight = outputDataHeight;
+
+function controlsReducer(state, action) {
+  switch (action.type) {
+    case "UPDATE_INPUT_TYPE": {
+      const { inputType } = action;
+
+      return {
+        ...state,
+        inputType
+      };
+    }
+
+    case "UPDATE_CHANNELS": {
+      const { hasRedChannel, hasGreenChannel, hasBlueChannel } = action;
+
+      return {
+        ...state,
+        hasRedChannel:
+          hasRedChannel == null ? state.hasRedChannel : hasRedChannel,
+        hasGreenChannel:
+          hasGreenChannel == null ? state.hasGreenChannel : hasGreenChannel,
+        hasBlueChannel:
+          hasBlueChannel == null ? state.hasBlueChannel : hasBlueChannel
+      };
+    }
+
+    case "UPDATE_LAYER_TYPE": {
+      const { layerType } = action;
+      return {
+        ...state,
+        layerType
+      };
+    }
+
+    case "UPDATE_CONV_STRIDE": {
+      const { convStride } = action;
+
+      return {
+        ...state,
+        convStride
+      };
+    }
+
+    case "UPDATE_CONV_FILTER_INDEX": {
+      const { convFilters, convStride } = state;
+      const { convFilterIndex } = action;
+      const newConvStride = Math.min(
+        convStride,
+        convFilters[convFilterIndex].filterSize
+      );
+
+      return {
+        ...state,
+        convFilterIndex,
+        convStride: newConvStride
+      };
+    }
+
+    case "UPDATE_CONV_FILTER_MATRIX": {
+      const { convFilters } = state;
+      const { convFilterIndex, filter, errors } = action;
+
+      if (!convFilters[convFilterIndex].isEditable) {
+        return state;
+      }
+
+      const filterSize = Math.sqrt(filter.length);
+      const newConvFilters = [...convFilters];
+
+      newConvFilters[convFilterIndex] = {
+        ...newConvFilters[convFilterIndex],
+        filter,
+        filterSize,
+        errors
+      };
+
+      if (errors.some(error => error === true)) {
+        return {
+          ...state,
+          convFilters: newConvFilters
+        };
+      }
+
+      const { convStride, scale } = state;
+      const newConvStride = Math.min(convStride, filterSize);
+      const newConvPadding = getConvPadding({
+        convFilters: newConvFilters,
+        convFilterIndex,
+        convStride: newConvStride
+      });
+      const {
+        outputWidth: outputDataWidth,
+        outputHeight: outputDataHeight
+      } = getOutputDimensions({
+        inputWidth: INPUT_DISPLAY_WIDTH / scale,
+        inputHeight: INPUT_DISPLAY_HEIGHT / scale,
+        filterSize,
+        padding: newConvPadding,
+        stride: newConvStride
+      });
+
+      return {
+        ...state,
+        convFilters: newConvFilters,
+        convStride: newConvStride,
+        convPadding: newConvPadding,
+        outputDataWidth,
+        outputDataHeight
+      };
+
+      return state;
+    }
+
+    case "UPDATE_POOL_FILTER_SIZE": {
+      const { poolFilterSize } = action;
+      const { poolStride, scale } = state;
+      const {
+        outputWidth: outputDataWidth,
+        outputHeight: outputDataHeight
+      } = getOutputDimensions({
+        inputWidth: INPUT_DISPLAY_WIDTH / scale,
+        inputHeight: INPUT_DISPLAY_HEIGHT / scale,
+        filterSize: poolFilterSize,
+        padding: 0,
+        stride: poolStride
+      });
+
+      return {
+        ...state,
+        poolFilterSize,
+        outputDataWidth,
+        outputDataHeight
+      };
+    }
+
+    case "UPDATE_POOL_STRIDE": {
+      const { poolStride } = action;
+      const { poolFilterSize, scale } = state;
+      const {
+        outputWidth: outputDataWidth,
+        outputHeight: outputDataHeight
+      } = getOutputDimensions({
+        inputWidth: INPUT_DISPLAY_WIDTH / scale,
+        inputHeight: INPUT_DISPLAY_HEIGHT / scale,
+        filterSize: poolFilterSize,
+        padding: 0,
+        stride: poolStride
+      });
+
+      return {
+        ...state,
+        poolStride,
+        outputDataWidth,
+        outputDataHeight
+      };
+    }
+
+    default: {
+      return state;
+    }
+  }
+}
+
+export { initialControlsState, controlsReducer };
---
+++ utils/controlsReducer.test.js
@@ -0,0 +1,359 @@
+import { INPUT_TYPES, LAYER_TYPES } from "./constants";
+import { initialControlsState, controlsReducer } from "./controlsReducer";
+
+function expectEnum(ENUM_OBJECT) {
+  return expect.stringMatching(
+    new RegExp("^" + Object.values(ENUM_OBJECT).join("|") + "$")
+  );
+}
+
+it("initial state has the right shape", () => {
+  expect(initialControlsState).toEqual(
+    expect.objectContaining({
+      inputType: expectEnum(INPUT_TYPES),
+      inputImage: expect.objectContaining({
+        src: expect.any(String),
+        scale: expect.any(Number)
+      }),
+      hasRedChannel: expect.any(Boolean),
+      hasGreenChannel: expect.any(Boolean),
+      hasBlueChannel: expect.any(Boolean),
+      layerType: expectEnum(LAYER_TYPES),
+      convFilters: expect.arrayContaining([
+        expect.objectContaining({
+          name: expect.any(String),
+          isEditable: expect.any(Boolean),
+          filter: expect.arrayContaining([expect.any(Number)]),
+          filterSize: expect.any(Number),
+          errors: expect.arrayContaining([expect.any(Boolean)])
+        })
+      ]),
+      convFilterIndex: expect.any(Number),
+      convStride: expect.any(Number),
+      poolFilterSize: expect.any(Number),
+      poolStride: expect.any(Number),
+      convPadding: expect.any(Number),
+      scale: expect.any(Number),
+      outputDataWidth: expect.any(Number),
+      outputDataHeight: expect.any(Number)
+    })
+  );
+});
+
+const testInitialState = {
+  inputType: INPUT_TYPES.IMAGE,
+  inputImage: {
+    src: "some src",
+    scale: 1
+  },
+  hasRedChannel: true,
+  hasGreenChannel: true,
+  hasBlueChannel: true,
+  layerType: LAYER_TYPES.CONV,
+  convFilters: [
+    {
+      name: "3x3 filter",
+      isEditable: false,
+      // prettier-ignore
+      filter: [
+        1, 2, 3,
+        4, 5, 6,
+        7, 8, 9
+      ],
+      filterSize: 3,
+      // prettier-ignore
+      errors: [
+        false, false, false,
+        false, false, false,
+        false, false, false
+      ]
+    },
+    {
+      name: "5x5 filter",
+      isEditable: true,
+      // prettier-ignore
+      filter: [
+        -0.1, -0.2, -0.3, -0.4,  -0.5,
+         0.6,  0.7,  0.8,  0.9,     1,
+           0,   10,   20,   30,    40,
+         -15,  -20,  -25,   80,   100,
+       14.98, 18.2, -2.4, 0.02, -0.01
+      ],
+      filterSize: 5,
+      // prettier-ignore
+      errors: [
+        false, false, false, false, false,
+        false, false, false, false, false,
+        false, false, false, false, false,
+        false, false, false, false, false,
+        false, false, false, false, false,
+      ]
+    }
+  ],
+  convFilterIndex: 0,
+  convStride: 1,
+  poolFilterSize: 2,
+  poolStride: 2,
+  convPadding: 1,
+  scale: 1,
+  outputDataWidth: 512,
+  outputDataHeight: 384
+};
+
+describe("UPDATE_INPUT_TYPE", () => {
+  it("updates input type", () => {
+    expect(
+      controlsReducer(testInitialState, {
+        type: "UPDATE_INPUT_TYPE",
+        inputType: INPUT_TYPES.CAMERA
+      }).inputType
+    ).toBe(INPUT_TYPES.CAMERA);
+  });
+});
+
+describe("UPDATE_CHANNELS", () => {
+  it("updates a single channel", () => {
+    expect(
+      controlsReducer(testInitialState, {
+        type: "UPDATE_CHANNELS",
+        hasRedChannel: false
+      })
+    ).toEqual(
+      expect.objectContaining({
+        hasRedChannel: false,
+        hasGreenChannel: true,
+        hasBlueChannel: true
+      })
+    );
+  });
+
+  it("updates multiple channels", () => {
+    expect(
+      controlsReducer(testInitialState, {
+        type: "UPDATE_CHANNELS",
+        hasGreenChannel: false,
+        hasBlueChannel: false
+      })
+    ).toEqual(
+      expect.objectContaining({
+        hasRedChannel: true,
+        hasGreenChannel: false,
+        hasBlueChannel: false
+      })
+    );
+  });
+});
+
+describe("UPDATE_LAYER_TYPE", () => {
+  it("updates layer type", () => {
+    expect(
+      controlsReducer(testInitialState, {
+        type: "UPDATE_LAYER_TYPE",
+        layerType: LAYER_TYPES.POOL
+      }).layerType
+    ).toBe(LAYER_TYPES.POOL);
+  });
+});
+
+describe("UPDATE_CONV_STRIDE", () => {
+  it("updates conv stride", () => {
+    expect(
+      controlsReducer(testInitialState, {
+        type: "UPDATE_CONV_STRIDE",
+        convStride: 2
+      }).convStride
+    ).toBe(2);
+  });
+});
+
+describe("UPDATE_CONV_FILTER_INDEX", () => {
+  it("updates conv filter index", () => {
+    expect(
+      controlsReducer(testInitialState, {
+        type: "UPDATE_CONV_FILTER_INDEX",
+        convFilterIndex: 1
+      })
+    ).toMatchObject({
+      convFilterIndex: 1
+    });
+  });
+
+  it("when the stride becomes invalid, updates the stride to the new max possible", () => {
+    expect(
+      controlsReducer(
+        {
+          ...testInitialState,
+          convFilterIndex: 1,
+          convStride: 5,
+          convPadding: 0
+        },
+        {
+          type: "UPDATE_CONV_FILTER_INDEX",
+          convFilterIndex: 0
+        }
+      )
+    ).toMatchObject({
+      convFilterIndex: 0,
+      convStride: 3
+    });
+  });
+});
+
+describe("UPDATE_CONV_FILTER_MATRIX", () => {
+  it("does not update the conv filter when the filter is not editable", () => {
+    expect(
+      controlsReducer(testInitialState, {
+        type: "UPDATE_CONV_FILTER_MATRIX",
+        convFilterIndex: 0,
+        // prettier-ignore
+        filter: [
+          1, 2, 3,
+          4, 5, 6,
+          7, 8, 100
+        ],
+        // prettier-ignore
+        errors: [
+          false, false, false,
+          false, false, false,
+          false, false, false
+        ]
+      }).convFilters[0]
+    ).toEqual(testInitialState.convFilters[0]);
+  });
+
+  it("updates conv filter with errors", () => {
+    expect(
+      controlsReducer(testInitialState, {
+        type: "UPDATE_CONV_FILTER_MATRIX",
+        convFilterIndex: 1,
+        // prettier-ignore
+        filter: [
+           -0.1, -0.2, -0.3, -0.4,  -0.5,
+            0.6,  0.7,  0.8,  0.9,     1,
+              0,   10,   20,   30,    40,
+            -15,  -20,  -25,   80,   100,
+          14.98, 18.2, -2.4, 0.02, "-0.01abc"
+        ],
+        // prettier-ignore
+        errors: [
+          false, false, false, false, false,
+          false, false, false, false, false,
+          false, false, false, false, false,
+          false, false, false, false, false,
+          false, false, false, false, true,
+        ]
+      }).convFilters[1]
+    ).toEqual({
+      ...testInitialState.convFilters[1],
+      // prettier-ignore
+      filter: [
+         -0.1, -0.2, -0.3, -0.4,  -0.5,
+          0.6,  0.7,  0.8,  0.9,     1,
+            0,   10,   20,   30,    40,
+          -15,  -20,  -25,   80,   100,
+        14.98, 18.2, -2.4, 0.02, "-0.01abc"
+      ],
+      // prettier-ignore
+      errors: [
+        false, false, false, false, false,
+        false, false, false, false, false,
+        false, false, false, false, false,
+        false, false, false, false, false,
+        false, false, false, false, true,
+      ]
+    });
+  });
+
+  it("updates conv filter and other affected parameters", () => {
+    const newState = controlsReducer(
+      {
+        ...testInitialState,
+        convFilterIndex: 1,
+        convStride: 5,
+        convPadding: 0
+      },
+      {
+        type: "UPDATE_CONV_FILTER_MATRIX",
+        convFilterIndex: 1,
+        // prettier-ignore
+        filter: [
+          10, 20, 30,
+          40, 50, 60,
+          70, 80, 90
+        ],
+        // prettier-ignore
+        errors: [
+          false, false, false,
+          false, false, false,
+          false, false, false
+        ]
+      }
+    );
+
+    expect(newState.convFilters[1]).toEqual({
+      ...testInitialState.convFilters[1],
+      // prettier-ignore
+      filter: [
+        10, 20, 30,
+        40, 50, 60,
+        70, 80, 90
+      ],
+      filterSize: 3,
+      // prettier-ignore
+      errors: [
+        false, false, false,
+        false, false, false,
+        false, false, false
+      ]
+    });
+
+    expect(newState).toMatchObject({
+      convStride: 3,
+      convPadding: 0,
+      outputDataWidth: 170,
+      outputDataHeight: 128
+    });
+  });
+});
+
+describe("UPDATE_POOL_FILTER_SIZE", () => {
+  it("updates pool filter size", () => {
+    expect(
+      controlsReducer(
+        {
+          ...testInitialState,
+          layerType: LAYER_TYPES.POOL
+        },
+        {
+          type: "UPDATE_POOL_FILTER_SIZE",
+          poolFilterSize: 3
+        }
+      )
+    ).toMatchObject({
+      poolFilterSize: 3,
+      outputDataWidth: 255,
+      outputDataHeight: 191
+    });
+  });
+});
+
+describe("UPDATE_POOL_STRIDE", () => {
+  it("updates pool stride", () => {
+    expect(
+      controlsReducer(
+        {
+          ...testInitialState,
+          layerType: LAYER_TYPES.POOL
+        },
+        {
+          type: "UPDATE_POOL_STRIDE",
+          poolStride: 1
+        }
+      )
+    ).toMatchObject({
+      poolStride: 1,
+      outputDataWidth: 511,
+      outputDataHeight: 383
+    });
+  });
+});
