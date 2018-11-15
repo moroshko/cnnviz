a1fe899be47a513945d62c115d5abd7054acad93
Misha Moroshko <michael.moroshko@gmail.com>
2018-11-15 12:41:13 -0800

    Implement gaussianBlur

---
+++ utils/filters.js
@@ -0,0 +1,25 @@
+const memoize = require('lodash.memoize');
+
+const gaussianBlur = memoize((size, sigma) => {
+  const twoSigmaSquare = 2 * sigma * sigma;
+  const divisor = Math.PI * twoSigmaSquare;
+  const m = (size - 1) >> 1;
+  const kernel = [];
+
+  for (let x = 0; x < size; x++) {
+    for (let y = 0; y < size; y++) {
+      const squareDistance = (x - m) * (x - m) + (y - m) * (y - m);
+      const gaussian = Math.exp(-squareDistance / twoSigmaSquare) / divisor;
+
+      kernel.push(gaussian);
+    }
+  }
+
+  const sum = kernel.reduce((acc, gaussian) => acc + gaussian);
+
+  return kernel.map(gaussian => gaussian / sum);
+});
+
+module.exports = {
+  gaussianBlur,
+};
